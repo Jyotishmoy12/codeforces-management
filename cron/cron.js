f92e360e7a1915894070dde561b4effe49d721f1
Jyotishmoy12 <93263133+Jyotishmoy12@users.noreply.github.com>
2025-06-16 11:49:27 +0530

    changed to 2 AM again

--- cron/cron.js
+++ cron/cron.js
@@ -2,9 +2,6 @@ import cron from 'node-cron';
 import rundailySync from './syncStudents.js';
 console.log("🕑 Cron job scheduled to run every day at 2 AM");
 // Run every day at 2 AM
-// Run every minute (for testing)
-cron.schedule('* * * * *', async () => {
-  console.log("🔔 Cron job triggered");
+cron.schedule('0 2 * * *', async () => {
   await rundailySync();
 });
-
