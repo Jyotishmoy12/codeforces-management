53f37ff82943ccfa36fd1326e65e96c6615aab46
Jyotishmoy12 <93263133+Jyotishmoy12@users.noreply.github.com>
2025-06-16 11:40:48 +0530

    testing cron job

--- cron/cron.js
+++ cron/cron.js
@@ -1,7 +1,10 @@
 import cron from 'node-cron';
 import rundailySync from './syncStudents.js';
-
+console.log("🕑 Cron job scheduled to run every day at 2 AM");
 // Run every day at 2 AM
-cron.schedule('0 2 * * *', async () => {
+// Run every minute (for testing)
+cron.schedule('* * * * *', async () => {
+  console.log("🔔 Cron job triggered");
   await rundailySync();
 });
+
